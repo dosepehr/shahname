d8fe604025facd1bfbeee6de4ec2bb5904595d8f
dosepehr <dosepehr@gmail.com>
2024-08-05 16:16:56 +0330

    add Character component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
+import Character from "@/components/Character";
+
 
 const Home = async () => {
     const res = await fetch("http://localhost:4000/characters");
@@ -8,32 +8,7 @@ const Home = async () => {
         <>
             <div className="w-[90%] mx-auto ">
                 {characters.map((char) => (
-                    <div className="bg-white/15 p-14 flex gap-x-14 rounded-[40px] flex-col lg:flex-row relative">
-                        <div className="min-w-[444px] h-[444px] w-1/2 lg:w-full mx-auto">
-                            <Image
-                                src={
-                                    "http://localhost:4000/images/" + char.cover
-                                }
-                                width={2000}
-                                height={2000}
-                                className="w-full h-full object-contain mx-auto"
-                            />
-                        </div>
-                        <div className="mt-14">
-                            <p className="font-bold text-3xl ">
-                                {+char.id < 10 && "0"}
-                                {char.id}. {char.name.en}
-                            </p>
-                            <p className="my-14">{char.description.en}</p>
-                            <Link
-                                href={char.id}
-                                className=" border border-white bg-transparent text-white py-3 px-2 rounded-xl"
-                            >
-                                see more
-                            </Link>
-                        </div>
-                        <p className="opacity-20 text-white absolute right-12 bottom-12" >Šāhnāmeh (The Book of Kings)</p>
-                    </div>
+                    <Character char={char} key={char.id} />
                 ))}
             </div>
         </>
---
+++ src/components/Character.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+const Character = ({ char }) => {
+    return (
+        <div>
+            <div className="bg-white/15 p-14 flex gap-x-14 rounded-[40px] flex-col lg:flex-row relative">
+                <div className="min-w-[444px] h-[444px] w-1/2 lg:w-full mx-auto">
+                    <Image
+                        src={"http://localhost:4000/images/" + char.cover}
+                        width={2000}
+                        height={2000}
+                        className="w-full h-full object-contain mx-auto"
+                    />
+                </div>
+                <div className="mt-14">
+                    <p className="font-bold text-3xl ">
+                        {+char.id < 10 && "0"}
+                        {char.id}. {char.name.en}
+                    </p>
+                    <p className="my-14">{char.description.en}</p>
+                    <Link
+                        href={char.id}
+                        className=" border border-white bg-transparent text-white py-3 px-2 rounded-xl"
+                    >
+                        see more
+                    </Link>
+                </div>
+                <p className="opacity-20 text-white absolute right-12 bottom-12">
+                    Šāhnāmeh (The Book of Kings)
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Character;
